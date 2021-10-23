a0fd055e315001d68713ba7118451da77b8f1181
ZhouRenGui <729049182@qq.com>
2021-10-23 13:42:35 +0800

    update reduce

--- reduce.js
+++ reduce.js
@@ -1,9 +1,19 @@
 // 手写 reduce
 
-Array.prototype.reduce = function (handler, init) {
-  let res = init;
-  for (const item of this) {
-    res = handler(res, item);
+Array.prototype.reduce = function (callback, initState) {
+  if (typeof callback !== "function") {
+    throw new TypeError("callback is required a function");
   }
+
+  if (!Array.isArray(this)) {
+    throw new TypeError("callee is required a array");
+  }
+
+  let res = initState;
+
+  for (let i = 0; i < this.length; i++) {
+    res = callback.call(this, res, this[i], i, this);
+  }
+
   return res;
 };
