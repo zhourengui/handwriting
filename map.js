76d55a27efaf87a65ca753b4a818c4f15657014c
ZhouRenGui <729049182@qq.com>
2021-10-23 13:45:38 +0800

    update map

--- map.js
+++ map.js
@@ -1,9 +1,19 @@
 // 手写map
 
 Array.prototype.map = function (callback) {
+  if (typeof callback !== "function") {
+    throw new TypeError("callback is required a function");
+  }
+
+  if (!Array.isArray(this)) {
+    throw new TypeError("callee is required a array");
+  }
+
   const res = [];
+
   for (let i = 0; i < this.length; i++) {
     res.push(callback.call(this, this[i], i, this));
   }
+
   return res;
 };
